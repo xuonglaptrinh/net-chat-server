b17f4c1526ec7e06fa470b2fbb0e50bb79dbf98c
NgaNguyenDuy <kyo1508@gmail.com>
2014-08-26 22:59:36 +0700

    step 5

---
+++ net_chat_server_step_1.js
@@ -0,0 +1,15 @@
+//net_chat_server_step_1
+
+var net = require('net'),
+  utils = require('./src/utils_step_1'),
+  chatServer = net.createServer();
+
+chatServer.on('connection', function(client) {
+  client.write('Hi!\n');
+  client.write('Bye!\n');
+  client.end()
+})
+
+chatServer.listen(9000, function(){
+  utils.writeLog(utils.getDateTimeStr() + ': net chat server listen on port 9000');
+});
